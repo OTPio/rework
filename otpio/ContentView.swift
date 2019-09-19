b2195aece029cb411df59ef5e26c447877ba8fc6
Mason Phillips <math.matrix@icloud.com>
2019-09-18 22:39:20 -0500

    Initial Commit

---
+++ otpio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  otpio
+//
+//  Created by Mason Phillips on 9/18/19.
+//  Copyright © 2019 Matrix Studios. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
